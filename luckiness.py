25c81cf38f67c9c843b89de9dc262946dabe09e7
Naveen <56919721+unaveenj@users.noreply.github.com>
2023-12-23 13:24:30 +0800

    Updated to zcore for luckiness index

--- luckiness.py
+++ luckiness.py
@@ -1,20 +1,26 @@
 import pandas as pd
 
 # Load the data
-df = pd.read_csv('toto_complete_address_v2.csv')  # Replace with the path to your file
+df = pd.read_csv('toto_complete_address_v3.csv')  # Replace with the path to your file
 
-# Convert Group 1 and Group 2 wins to numeric values in case they are not
+# Convert Group 1 and Group 2 wins to numeric values
 df['Group 1 Wins'] = pd.to_numeric(df['Group 1 Wins'], errors='coerce')
 df['Group 2 Wins'] = pd.to_numeric(df['Group 2 Wins'], errors='coerce')
 
-# Calculate percentile ranks for Group 1 and Group 2 wins
-df['Group 1 Percentile Rank'] = df['Group 1 Wins'].rank(pct=True) * 100
-df['Group 2 Percentile Rank'] = df['Group 2 Wins'].rank(pct=True) * 100
+# Calculate the mean and standard deviation for Group 1 and Group 2 wins
+mean_g1 = df['Group 1 Wins'].mean()
+std_g1 = df['Group 1 Wins'].std()
+mean_g2 = df['Group 2 Wins'].mean()
+std_g2 = df['Group 2 Wins'].std()
 
-# Calculate average percentile rank and scale it to 0-10 for Luckiness score
-df['Luckiness'] = ((df['Group 1 Percentile Rank'] + df['Group 2 Percentile Rank']) / 2) / 10
+# Calculate the Z-Scores
+df['Group 1 Z-Score'] = (df['Group 1 Wins'] - mean_g1) / std_g1
+df['Group 2 Z-Score'] = (df['Group 2 Wins'] - mean_g2) / std_g2
 
-# Save the DataFrame with the new 'Luckiness' column as 'toto2.csv'
-df.to_csv('toto3.csv', index=False)
+# Calculate the Luckiness Index
+df['Luckiness Index'] = (df['Group 1 Z-Score'] + df['Group 2 Z-Score']) / 2
 
-print("Data processed and saved as 'toto3.csv'")
+# Save the DataFrame with the new 'Luckiness Index' column as 'toto3.csv'
+df.to_csv('toto4.csv', index=False)
+
+print("Data processed and saved as 'toto4.csv'")

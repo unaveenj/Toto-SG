eb619e194f3d429986b3d89a7fe4ed13d71affbe
Naveen <56919721+unaveenj@users.noreply.github.com>
2023-12-30 21:41:13 +0800

    File naming editted

--- luckiness.py
+++ luckiness.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import datetime
 
-# Load the data
-df = pd.read_csv('toto_complete_address_v3.csv')  # Replace with the path to your file
+
+df = pd.read_csv('toto_30-12-2023.csv')  # Replace with the path to your file
 
 # Convert Group 1 and Group 2 wins to numeric values
 df['Group 1 Wins'] = pd.to_numeric(df['Group 1 Wins'], errors='coerce')
@@ -20,7 +21,12 @@ df['Group 2 Z-Score'] = (df['Group 2 Wins'] - mean_g2) / std_g2
 # Calculate the Luckiness Index
 df['Luckiness Index'] = (df['Group 1 Z-Score'] + df['Group 2 Z-Score']) / 2
 
-# Save the DataFrame with the new 'Luckiness Index' column as 'toto3.csv'
-df.to_csv('toto4.csv', index=False)
 
-print("Data processed and saved as 'toto4.csv'")
+current_date = datetime.datetime.now()
+formatted_date = current_date.strftime("%d-%m-%Y")
+file_name = f"toto_{formatted_date}_luckiness.csv"
+
+
+df.to_csv(f'{file_name}', index=False)
+
+print(f"Data processed and saved as '{file_name}'")
